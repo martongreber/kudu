017d12fd25ac4b2c11a5d7f8cd8279a5d1c82c57
Agnes Tevesz <tevesz@cloudera.com>
2019-07-31 16:09:45 +0200

    CLOUDERA_BUILD RELENG-6207: add CSD build script
    Also disable signing of all artifacts built by Gradle.
    
    Change-Id: I26ea6ed89da4870cc8151adb28da48468a39204c

---
+++ cloudera/build_csd.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+#
+# Script that builds Kudu CSDs.
+
+set -ex
+
+CSD_BUILD_DIR=${1:-"./build/csds"}
+
+pushd java
+
+# First, build the CSDs.
+CSD_TASKS=$(find kudu-csd* -type d -maxdepth 0 -exec echo {}:assemble \; | tr '\n' ' ')
+GRADLE_ARGS="--console=plain --no-daemon"
+echo "Running the following CSD tasks: $CSD_TASKS"
+./gradlew -PbuildCSD -PskipSigning $GRADLE_ARGS $CSD_TASKS
+
+# Next, install them into the CSD repo.
+mkdir -p ${CSD_BUILD_DIR}
+for JAR in kudu-csd*/build/libs/*.jar; do
+  if [[ "$JAR" =~ "tests" ]]; then
+    # Ignore test JARs.
+    continue
+  fi
+
+  # Need to override packaging from 'pom' to 'jar' because Maven!
+  mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file \
+    -Dfile="$JAR" \
+    -DlocalRepositoryPath="${CSD_BUILD_DIR}" \
+    -Dpackaging=jar \
+    -DpomFile="$(dirname $JAR)/../pom.xml"
+done
+popd
+
+

martongreber/kudu

commit bc9b964f8c9ff98b79d91258bb78163516807423

CDPD-21419 Fix keystore format
JCE keystores are not secure and they shouldn't be used, especially
in FIPS clusters. CM exposes a config to use BouncyCastle keystores
(BCFKS), which was adhered to by Kudu everywhere, except for one file:
the master TLS truststore, which is currently used by Ranger, is
password-protected and the password is stored in a keystore. This
keystore was hard-coded to be JCEKS. This Commit changes the script to
generate this keystore in the configured format.

The truststore itself is still JCEKS, but that seems to be the case
CDP-wide for all truststores.

Change-Id: I09bde55f071dca82de631b7829b6bacb1d6fe24b

java/kudu-csd/src/descriptor/service.sdl
@@ -278,6 +278,7 @@
           "CORE_DUMP_DIRECTORY" : "${master_core_dump_directory}",
           "KUDU_TRUSTSTORE_LOCATION": "${ssl_client_truststore_location}",
           "KUDU_TRUSTORE_PASSWORD": "${ssl_client_truststore_password}",
+          "KUDU_KEYSTORE_TYPE": "${keystore_type}",
           "RANGER_SERVICE": "${dependency:RANGER}",
           "RANGER_AUDIT_BASE_PATH": "${ranger_base_audit_url}",
           "RANGER_KUDU_HDFS_AUDIT_DIR": "${ranger_kudu_plugin_hdfs_audit_directory}",

java/kudu-csd/src/scripts/kudu.sh
@@ -202,7 +202,8 @@ if [[ -n "${RANGER_SERVICE}" && "${RANGER_SERVICE}" != "none" ]]; then
   set +x
   if [ -n "${KUDU_TRUSTSTORE_LOCATION}" ] && [ -n "${KUDU_TRUSTORE_PASSWORD}" ]; then
     perl -pi -e "s#\{\{RANGER_PLUGIN_TRUSTSTORE}}#${KUDU_TRUSTSTORE_LOCATION}#g" "${CONF_DIR}"/ranger-kudu-policymgr-ssl.xml
-    RANGER_PLUGIN_TRUSTSTORE_CRED_FILE="jceks://file${CONF_DIR}/rangerpluginssl.jceks"
+    STORETYPE="${KUDU_KEYSTORE_TYPE:-jceks}"
+    RANGER_PLUGIN_TRUSTSTORE_CRED_FILE="${STORETYPE}://file${CONF_DIR}/rangerpluginssl.${STORETYPE}"
 
     # Use jars from Ranger admin package to generate the trsustore credential file.
     RANGER_ADMIN_CRED_LIB="${PARCELS_ROOT}/${PARCEL_DIRNAMES}/lib/ranger-admin/cred/lib/"

commit 42cbf57aba37919d1432bdbad93152f3b1c93093

Make Ranger client keystore always JCEKS or BCFKS
According to CSA-857, bc9b964 broke Ranger integration when the store
type is set to JKS, as Ranger's buildks tool apparently supports only
JCEKS and BCFKS.

As the point of making it possible to store the keys in BCFKS in FIPS
mode, this patch changes the behavior to always use/generate JCEKS
stores for the Ranger client keystore, except when the keystore type is
set to BCFKS, in which case BCFKS is used.

Change-Id: Ic0dc41d188f75a358ae5335f5db169994ddd49b1

java/kudu-csd/src/descriptor/service.sdl
@@ -278,7 +278,7 @@
           "CORE_DUMP_DIRECTORY" : "${master_core_dump_directory}",
           "KUDU_TRUSTSTORE_LOCATION": "${ssl_client_truststore_location}",
           "KUDU_TRUSTORE_PASSWORD": "${ssl_client_truststore_password}",
-          "KUDU_KEYSTORE_TYPE": "${keystore_type}",
+          "RANGER_KEYSTORE_TYPE": "${keystore_type}",
           "RANGER_SERVICE": "${dependency:RANGER}",
           "RANGER_AUDIT_BASE_PATH": "${ranger_base_audit_url}",
           "RANGER_KUDU_HDFS_AUDIT_DIR": "${ranger_kudu_plugin_hdfs_audit_directory}",

java/kudu-csd/src/scripts/kudu.sh
@@ -202,7 +202,12 @@ if [[ -n "${RANGER_SERVICE}" && "${RANGER_SERVICE}" != "none" ]]; then
   set +x
   if [ -n "${KUDU_TRUSTSTORE_LOCATION}" ] && [ -n "${KUDU_TRUSTORE_PASSWORD}" ]; then
     perl -pi -e "s#\{\{RANGER_PLUGIN_TRUSTSTORE}}#${KUDU_TRUSTSTORE_LOCATION}#g" "${CONF_DIR}"/ranger-kudu-policymgr-ssl.xml
-    STORETYPE="${KUDU_KEYSTORE_TYPE:-jceks}"
+    if [[ "${RANGER_KEYSTORE_TYPE}" -eq "bcfks" ]]
+    then
+      STORETYPE="bcfks"
+    else
+      STORETYPE="jceks"
+    fi
     RANGER_PLUGIN_TRUSTSTORE_CRED_FILE="${STORETYPE}://file${CONF_DIR}/rangerpluginssl.${STORETYPE}"
 
     # Use jars from Ranger admin package to generate the trsustore credential file.